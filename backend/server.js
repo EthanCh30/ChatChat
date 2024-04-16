996461bf48d119830335f08a89f0f1a3138bea6b
chen <565306120@qq.com>
2024-04-16 16:39:15 +0930

    update .gitignore and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express")
-
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => { 
     // root route http://localhost:5000/
     res.send("Hello World!");
 });
 
-app.listen(5000, () => console.log("Server Running on port 5000"));
+app.listen(PORT, () => console.log('Server Running on port ${PORT}'));

1e4de388c94b320e6f8dc18f3e182b02273f8371
chen <565306120@qq.com>
2024-04-16 17:02:04 +0930

    update server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+
 const app = express();
-
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => { 
     // root route http://localhost:5000/

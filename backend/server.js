896b4b287efaca6038289c1a0b42ba8b00ae3dba
chen <565306120@qq.com>
2024-04-18 22:46:27 +0930

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 dotenv.config();
@@ -9,16 +10,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/api/auth/signup", (req, res) => { 
-    console.log("signup route"); 
-});
-app.get("/api/auth/login", (req, res) => { 
-    console.log("login route"); 
-});
-app.get("/api/auth/logout", (req, res) => { 
-    console.log("logout route"); 
-});
-
-
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => console.log('Server Running on port ${PORT}'));

e7c37a74009385f59828bb4b88773bcd66bd7c72
chen <565306120@qq.com>
2024-04-25 13:55:02 +0930

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <p className="text-3xl font-medium text-sky-700">
-    Hello World
+    Hello World！
     <h1>Hi</h1>
    </p>
 
